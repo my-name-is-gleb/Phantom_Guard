549fc456f08caf3cf7b146cf842d301e4fb6e79c
Gleb <glebsemenenko04@gmail.com>
2026-01-07 14:19:43 +0200

    Clean commit of config example from VS Code

---
+++ PHANTOM_GUARD_V1/src_phantom/config_example.py
@@ -0,0 +1,21 @@
+"""EXAMPLE Configuration - Change values and save as config.py"""
+
+ADMIN_PASSWORD = "YOUR_SECRET_PHRASE_HERE"
+NUMBER_OF_ATTEMPTS = 5
+
+REQUIRED_AVG_SPEED = 0.2
+ALLOW_PASTE = False
+
+PRANK_MODE_ACTIVATED = True
+CAPTURE_INTERVAL = 15
+
+LOG_FILE = "vault/logs/security_audit.log"
+CAPTURES_DIR = "vault/captures/"
+GO_AGENT_EXE = "core/agent_go/watcher.exe"
+FAKE_DB = "vault/fake_secrets.json"
+
+VOICE_RATE = 150
+VOICE_VOLUME = 1.0
+
+GREETING_MSG = "Welcome message here"
+ALARM_MSG = "Breach message here"
